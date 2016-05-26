c46c51542dbae5d182fc6793f850574980d75248
UVADave <dpg3k@virginia.edu>
2016-05-26 12:12:23 -0400

    Add environment driven startup options

--- scripts/entry.sh
+++ scripts/entry.sh
@@ -1,6 +1,37 @@
-if [ -z "$DBPASSWD" ]; then
-   echo "ERROR: DBPASSWD must be defined"
-   exit 1
+# set blank options variables
+DBHOST_OPT=""
+DBNAME_OPT=""
+DBUSER_OPT=""
+DBPASSWD_OPT=""
+TOKENURL_OPT=""
+
+# database host
+if [ -n "$DBHOST" ]; then
+   DBHOST_OPT="--dbhost $DBHOST"
 fi
 
-bin/deposit-reg-ws --dbpassword $DBPASSWD
+# database name
+if [ -n "$DBNAME" ]; then
+   DBNAME_OPT="--dbname $DBNAME"
+fi
+
+# database user
+if [ -n "$DBUSER" ]; then
+   DBUSER_OPT="--dbuser $DBUSER"
+fi
+
+# database password
+if [ -n "$DBPASSWD" ]; then
+   DBPASSWD_OPT="--dbpassword $DBPASSWD"
+fi
+
+# token authentication service URL
+if [ -n "$TOKENAUTH_URL" ]; then
+   TOKENURL_OPT="--tokenauth $TOKENAUTH_URL"
+fi
+
+bin/deposit-reg-ws $DBHOST_OPT $DBNAME_OPT $DBUSER_OPT $DBPASSWD_OPT $TOKENURL_OPT
+
+#
+# end of file
+#
